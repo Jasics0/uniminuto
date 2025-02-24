3df493350166ca72304691bc637e8ae64c77f61f
Javier Charry <javier.charry@prodigio.tech>
2025-02-24 10:43:18 -0500

    first commit

---
+++ taller-2-uniminuto/src/main/java/com/nobugs/taller2uniminuto/Taller2UniminutoApplication.java
@@ -0,0 +1,13 @@
+package com.nobugs.taller2uniminuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller2UniminutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller2UniminutoApplication.class, args);
+    }
+
+}
