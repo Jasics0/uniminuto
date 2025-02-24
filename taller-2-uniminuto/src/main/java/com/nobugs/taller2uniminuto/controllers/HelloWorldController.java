82f6a9b2209ddf72950fbc084a855ee36535e331
Javier Charry <javier.charry@prodigio.tech>
2025-02-24 11:03:29 -0500

    Feat: Add hello world controller

---
+++ taller-2-uniminuto/src/main/java/com/nobugs/taller2uniminuto/controllers/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.nobugs.taller2uniminuto.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+
+    @GetMapping("/world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("")
+    public String hello() {
+        return "Hello!";
+    }
+
+}
